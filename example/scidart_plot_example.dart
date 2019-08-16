edc66f0c1bbd4a0d4fe324809a1e6c523c2fe2af
Ângelo Polotto <angelopolotto@yahoo.com.br>
2019-08-16 18:00:19 -0300

    initial project

---
+++ example/scidart_plot_example.dart
@@ -0,0 +1,6 @@
+import 'package:scidart_plot/scidart_plot.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/scidart_plot.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library scidart_plot;
+
+export 'src/scidart_plot_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
